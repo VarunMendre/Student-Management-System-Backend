fa97d74878d70dc85869900f21d59693cfb630a6
Varun Mendre <varunmm0404@gmail.com>
2026-05-26 13:39:47 +0530

    seed: seeding auth:db & documenting DB setup

--- seeds/seeds-auth.js
+++ seeds/seeds-auth.js
@@ -3,7 +3,7 @@ import { pool } from "../config/db.js";
 
 const seedPrincipal = async () => {
   try {
-    const hashedPassword = await bcrypt.hash("principal@123", 12);
+    const hashedPassword = await bcrypt.hash("Principal@123456", 12);
 
     await pool.query(
       `INSERT INTO app_users (name, email, password, contact_number, role, is_active, is_password_changed)
